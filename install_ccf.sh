abbca5125b7cf7f1db1297590f217842f22607fc
Alyanak <60792074+mAlyanak@users.noreply.github.com>
2021-11-12 10:21:15 +0100

    Changed a line

--- install_ccf.sh
+++ install_ccf.sh
@@ -1,11 +1,11 @@
-ccfversion=1.0.13
 
+#Declare what version of CCF you want to use
+ccfversion=1.0.13
 if [ -z "$1" ]
   then
     ccfversion=$1
 fi
 
-#Declare what version of CCF you want to use
 export CCF_VERSION=$ccfversion
 
 #Get the CCF binary
