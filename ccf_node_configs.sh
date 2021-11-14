d59528e4ce2c73f88f186bace32d565b1d41fc7b
Alyanak <60792074+mAlyanak@users.noreply.github.com>
2021-11-14 15:07:52 +0100

    Update ccf_node_configs.sh

--- ccf_node_configs.sh
+++ ccf_node_configs.sh
@@ -1,28 +1,34 @@
+sudo apt install net-tools
+cd ~
 mkdir ~/network
 mkdir ~/network/configurations
 mkdir ~/network/certificates
 
-cat <<EOT >> ~/network/configurations/start.ini
+PATH_HERE=$(pwd)
+PUB_IP=$(curl ifconfig.co)
+PRIV_IP=$(hostname -i)
+
+cat <<EOT >> ~/network/configurations/start_node.ini
 
 # Configuration for your enclave image and type. If you're not running SGX supported hardware
-# you would need to change these to 'libjs_generic.enclave.so.signed' and 'virtual' respectively
-enclave-file = ./libjs_generic.enclave.so.signed
+# you would need to change these to 'libjs_generic.virtual.so' and 'virtual' respectively
+enclave-file = ${PATH_HERE}/ccfrelease/lib/libjs_generic.enclave.so.signed
 enclave-type = release
 
 # Standard consensus algorithm used, can also make use of btf
 consensus = cft
 
 # Node and RPC addresses <localip><port>
-rpc-address = 10.0.0.18:443
-node-address = 10.0.0.18:6600
+rpc-address = ${PRIV_IP}:443
+node-address = ${PRIV_IP}:6600
 
 # Your public address, the one you usually access your vm with. <publicip><thesame port as rpc port>
-public-rpc-address = 20.108.20.207:443
+public-rpc-address = ${PUB_IP}:443
 
 # Locations where you will generate the ledger dir, cert and pid file
-ledger-dir = ./legder/
-node-cert-file = ./certificates/node_cert.pem
-node-pid-file = ./cchost.pid
+ledger-dir = ${PATH_HERE}/network/legder/
+node-cert-file = ${PATH_HERE}/network/certificates/node_cert.pem
+node-pid-file = ${PATH_HERE}/network/cchost.pid
 
 # Your DNS, if you have one.
 san = "dNSName:partical.uksouth.cloudapp.azure.com"
@@ -30,13 +36,17 @@ san = "dNSName:partical.uksouth.cloudapp.azure.com"
 [start]
 
 #Network-cert location
-network-cert-file = ./certificates/network_cert.pem
+network-cert-file = ${PATH_HERE}/network/certificates/network_cert.pem
 
 # Initial registered member (Unactivated!)
 # You can add more member-info fields to increase your initial members,
 # you do need new certificates for each new one though
-member-info = "./certificates/Martijn_cert.pem,./certificates/Martijn_enc_pubk.pem"
+member-info = "${PATH_HERE}/network/certificates/Member_cert.pem,${PATH_HERE}/network/certificates/Member_enc_pubk.pem"
 
 #Paths to your constitution
-constitution=[/home/mar/partical/constitution/actions.js,/home/mar/partical/constitution/validate.js,/home/mar//partical/constitution/resolve.js,/home/mar//partical/constitution/apply.js]
+constitution=[${PATH_HERE}/ccfrelease/bin/actions.js,${PATH_HERE}/ccfrelease/bin/validate.js,${PATH_HERE}/ccfrelease/bin/resolve.js,${PATH_HERE}/ccfrelease/bin/apply.js]
+EOT
+
+cat <<EOT >> ~/network/configurations/join_node.ini
+
 EOT
