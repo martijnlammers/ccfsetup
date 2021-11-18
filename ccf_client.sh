7ed150a6acad340027c77ceff05a7a14aebe912e
Alyanak <60792074+mAlyanak@users.noreply.github.com>
2021-11-18 14:03:28 +0100

    Update ccf_client.sh

--- ccf_client.sh
+++ ccf_client.sh
@@ -1,47 +1,24 @@
 mkdir ~/client
-cat <<EOT >> ~/client/sample.js
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-exports.__esModule = true;
+sudo apt install node-typescript
+cat <<EOT >> ~/client/sample.ts
 //File system access is necessary to read the certificates.
-var fs = require("fs");
-var https = require("https");
-var options = {
+import fs = require('fs');
+import https = require('https');
+
+
+//Request option schema. Can be expanded with more options.
+interface RequestOptions {
+    hostname: string,
+    port: number,
+    path: string,
+    method: string,
+    ca?: Buffer,
+    key?: Buffer,
+    cert?: Buffer
+    header: object,
+}
+
+const options: RequestOptions = {
     hostname: 'particaldemo1.uksouth.cloudapp.azure.com',
     port: 443,
     path: '/app/sample',
@@ -52,45 +29,46 @@ var options = {
     header: {
         'Content-Type': 'application/json'
     }
-};
-var data = JSON.parse('{"someField":"someValue"}');
-function send_https_req(options, json) {
-    function promiseHandler(thenFunc, catchFunc) {
-        var request = https.request(options, function (response) {
+}
+
+const data: Buffer = JSON.parse('{"someField":"someValue"}');
+
+
+
+function send_https_req(options: RequestOptions, json: Buffer): Promise<string> {
+    function promiseHandler(thenFunc: (value: string) => void, catchFunc: (error_name: Error) => void) {
+        const request = https.request(options, (response) => {
             response.setEncoding('utf8');
-            response.on('data', function (resdata) {
+            response.on('data', (resdata) => {
+
                 //Process the response
                 thenFunc(resdata);
             });
         });
+
         // Write data to request body
         request.write(json);
         request.end();
     }
-    var p = new Promise(promiseHandler);
+
+    const p: Promise<string> = new Promise(promiseHandler);
     return p;
 }
+
+
 //You can make a custom function out of this. It has to be async to easily use the promise in the function.
-function output_response(options, json) {
-    return __awaiter(this, void 0, void 0, function () {
-        var response, error_1;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 2, , 3]);
-                    return [4 /*yield*/, send_https_req(options, json)];
-                case 1:
-                    response = _a.sent();
-                    console.log(response);
-                    return [3 /*break*/, 3];
-                case 2:
-                    error_1 = _a.sent();
-                    console.log(error_1);
-                    return [3 /*break*/, 3];
-                case 3: return [2 /*return*/];
-            }
-        });
-    });
+async function output_response(options: RequestOptions, json: Buffer) {
+
+    //Sending requests isn't guarrenteed, hence the promise/trycatch
+    try {
+        const response = await send_https_req(options, json);
+        console.log(response);
+
+    } catch(error){
+        console.log(error);
+    }  
 }
+
 output_response(options, data);
+
 EOT
