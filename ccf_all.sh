74ed33b3375bb4ae6670df756e803fcf1a1c53fa
Alyanak <60792074+mAlyanak@users.noreply.github.com>
2021-11-19 14:18:05 +0100

    Update ccf_all.sh

--- ccf_all.sh
+++ ccf_all.sh
@@ -1,4 +1,6 @@
 # Shell to automatically deploy template app/network
+# Important to note that you still need to reconfigure some settings as thing like 
+# your DNS cannot be configured generically.
 bash ~/ccfsetup/ccf_install.sh
 bash ~/ccfsetup/ccf_app_setup.sh
 bash ~/ccfsetup/ccf_app_config.sh
