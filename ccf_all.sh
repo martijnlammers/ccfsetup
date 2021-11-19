2ae3ebdfb56c351d1e34d3b1cc80fae72f1ade27
Alyanak <60792074+mAlyanak@users.noreply.github.com>
2021-11-19 14:03:59 +0100

    Create ccf_all.sh
    Necessary for easy redeployment on different machines if guide is already known

---
+++ ccf_all.sh
@@ -0,0 +1,5 @@
+# Shell to automatically deploy template app/network
+bash ~/ccfsetup/ccf_install.sh
+bash ~/ccfsetup/ccf_app_setup.sh
+bash ~/ccfsetup/ccf_app_config.sh
+bash ~/ccfsetup/ccf_node_configs.sh
