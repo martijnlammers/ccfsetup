039dd6a24c70feea3ab15cb28fda6a373b810778
Alyanak <60792074+mAlyanak@users.noreply.github.com>
2021-11-19 14:15:06 +0100

    Update ccf_all.sh

--- ccf_all.sh
+++ ccf_all.sh
@@ -3,3 +3,11 @@ bash ~/ccfsetup/ccf_install.sh
 bash ~/ccfsetup/ccf_app_setup.sh
 bash ~/ccfsetup/ccf_app_config.sh
 bash ~/ccfsetup/ccf_node_configs.sh
+cd ~/app_building_dir/
+npm install 
+npm run build
+
+cd ~/network/certificates
+./../../ccfrelease/bin/keygenerator.sh \
+--name Member \
+--gen-enc-key
