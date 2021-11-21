0aef658f316820a2e77582daaeca34347db0c461
Alyanak <60792074+mAlyanak@users.noreply.github.com>
2021-11-21 16:43:58 +0100

    Update ccf_all.sh

--- ccf_all.sh
+++ ccf_all.sh
@@ -1,15 +1,17 @@
+#!/bin/bash
 # Shell to automatically deploy template app/network
 # Important to note that you still need to reconfigure some settings as thing like 
 # your DNS arent being configured generically
-bash ~/ccfsetup/ccf_install.sh
-bash ~/ccfsetup/ccf_app_setup.sh
-bash ~/ccfsetup/ccf_app_config.sh
-bash ~/ccfsetup/ccf_node_configs.sh
-cd ~/app_building_dir/
+PATH_HERE=$(pwd)
+bash ${PATH_HERE}/ccfsetup/ccf_install.sh
+bash ${PATH_HERE}/ccfsetup/ccf_app_setup.sh
+bash ${PATH_HERE}/ccfsetup/ccf_app_config.sh
+bash ${PATH_HERE}/ccfsetup/ccf_node_configs.sh
+cd ${PATH_HERE}/app_building_dir/
 npm install 
 npm run build
 
-cd ~/network/certificates
-./../../ccfrelease/bin/keygenerator.sh \
+cd ${PATH_HERE}/network/certificates
+${PATH_HERE}/ccfrelease/bin/keygenerator.sh \
 --name Member \
 --gen-enc-key
